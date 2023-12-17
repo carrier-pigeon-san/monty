a9120df9ea0590cdfd176379eb95bff276de9a33
Sandy Obare <obaresandy@gmail.com>
2023-12-17 23:13:45 +0000

    swap() function error message newline addition

--- 3_func_lib.c
+++ 3_func_lib.c
@@ -10,7 +10,7 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		dprintf(2, "L%d: can't swap, stack too short", line_number);
+		dprintf(2, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	temp_node = *stack;
