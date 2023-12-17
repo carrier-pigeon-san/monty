58d0e58d6d849cff30ada881db7000ecb98afe6f
Sandy Obare <obaresandy@gmail.com>
2023-12-17 21:44:12 +0000

    pint() empty stack error message newline addition

--- 2_func_lib.c
+++ 2_func_lib.c
@@ -75,7 +75,7 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		dprintf(2, "L%d: can't pint, stack empty", line_number);
+		dprintf(2, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
