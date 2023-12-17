b8c66fa3617330e8162e683cd8a453bae0b86aff
Sandy Obare <obaresandy@gmail.com>
2023-12-17 12:47:18 +0000

    exit with EXIT_FAILURE on invalid push opcode

--- opcode_parse.c
+++ opcode_parse.c
@@ -23,6 +23,7 @@ void parse_tokens(char **opcode_tokens, int line_number)
 			{
 				/*printf("route 1\n");*/
 				dprintf(2, "L%d: usage: push integer", line_number);
+				exit(EXIT_FAILURE);
 			}
 			fnc_def(&head, atoi(opcode_tokens[1]));
 		}
