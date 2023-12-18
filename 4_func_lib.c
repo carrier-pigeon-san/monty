392685e75e1e857364bfce4707cacdd895d37abd
Sandy Obare <obaresandy@gmail.com>
2023-12-18 00:42:31 +0000

    mod() function error message typo correction

--- 4_func_lib.c
+++ 4_func_lib.c
@@ -24,7 +24,7 @@ void mod(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		dprintf(2, "L%d: can't mul, stack too short\n", line_number);
+		dprintf(2, "L%d: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n == 0)
